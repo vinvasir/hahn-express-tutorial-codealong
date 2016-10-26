8a15dd2686ceb96c8e0dee81199d75939deb86fc
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-26 13:35:50 -0400

    used the morgan middleware for logging

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 var express = require('express');
+var logger = require('morgan');
 var http = require('http');
 
 var app = express();
 
-app.use(function(request, response, next) {
-	console.log("In comes a request to: " + request.url);
-	next();
-});
-
-app.use(function(request, response, next) {
-	var minute = (new Date()).getMinutes();
-	if (( minute % 2 ) === 0) {
-		next();
-	}
-	else {
-		response.statusCode = 403;
-		response.end("Not authorized.");
-	}
-});
+app.use(logger('short'));
 
 app.use(function(request, response) {
-	response.end("Secret info: the password is 'swordfish'!");
+	response.writeHead(200, { 'Content-Type': 'text/plain' })
+	response.end("Hello, world!");
 });
 
 http.createServer(app).listen(3000);
