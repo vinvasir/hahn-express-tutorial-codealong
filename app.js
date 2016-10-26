2d8200b02b7ce8bdfd50ef7cc2199cf7c9d9bdb0
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-26 13:26:33 -0400

    split the logging and response functionality into two different middleware functions

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var http = require('http');
 
 var app = express();
 
-app.use(function(request, response) {
+app.use(function(request, response, next) {
 	console.log("In comes a request to: " + request.url);
-	response.end("Hello, world");
+	next();
+});
+
+app.use(function(request, response) {
+	response.writeHead(200, { "Content-Type": "text/plain" });
+	response.end("Hello, world!");
 });
 
 http.createServer(app).listen(3000);
