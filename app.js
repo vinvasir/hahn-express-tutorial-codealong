e0275b6b036375d0380eab52f5fe1eadbc6050be
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-26 13:45:01 -0400

    used express's static module to load files in the public folder

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var logger = require('morgan');
+var path = require('path');
 var http = require('http');
 
 var app = express();
 
 app.use(logger('short'));
+
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
 
 app.use(function(request, response) {
 	response.writeHead(200, { 'Content-Type': 'text/plain' })
