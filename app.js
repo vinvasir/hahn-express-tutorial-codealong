009e96981d8a68a512c017a433f4ca19912a2b30
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-26 13:48:41 -0400

    wrote basic routes

--- app.js
+++ app.js
@@ -10,9 +10,21 @@ app.use(logger('short'));
 var publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
+app.get('/', function(request, response) {
+	response.end("Welcome to my homepage!");
+});
+
+app.get('/about', function(request, response) {
+	response.end("Welcome to the about page!");
+});
+
+app.get('/weather', function(request, response) {
+	response.end("The current weather is NICE");
+});
+
 app.use(function(request, response) {
-	response.writeHead(200, { 'Content-Type': 'text/plain' })
-	response.end("Hello, world!");
+	response.statusCode = 404;
+	response.end("Sorry, I couldn't find that page.");
 });
 
 http.createServer(app).listen(3000);
