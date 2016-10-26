31b4913da3fbf1c1dff2b104ac238726bb4fc5f5
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-10-26 13:14:02 -0400

    created basic node hello world server

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var url = require('url');
-var parsedURL = url.parse('http://www.example.com/profile?name=barry');
+var http = require('http');
 
-console.log(parsedURL.protocol); // "http:""
-console.log(parsedURL.host); // "www.example.com"
-console.log(parsedURL.query); // "name=barry"
+function requestHandler(request, response) {
+	console.log("In comes a request to: " + request.url);
+	response.end("Hello, world");
+}
+
+var server = http.createServer(requestHandler);
+
+server.listen(3000);
